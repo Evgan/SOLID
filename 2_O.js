64aa6d9869a52d37ce2ec49263166fbc167f7bd8
epenkrart <e.penkrat@whiteskydigital.com>
2021-03-24 00:05:42 +0300

    2_O not use Open Close Principle

--- 2_O.js
+++ 2_O.js
@@ -1,1 +1,44 @@
 // Open Close principle
+
+// Какието классы должны быть доступны для разширения но закрыты для модифицирования
+
+class Square {
+    constructor(size) {
+        this.type = 'Square';
+        this.size = size;
+    }
+}
+
+class Circle {
+    constructor(radius) {
+        this.type = 'Circle';
+        this.radius = radius
+    }
+}
+
+class AreaCalculator {
+    constructor(shapes = []) {
+        this.shapes = shapes
+    }
+
+    sum() {
+        return this.shapes.reduce((acc, shape) => {
+            if(shape.type === 'Circle') {
+                acc += (shape.radius ** 2) * Math.PI
+            } else if (shape.type === 'Square') {
+                acc += shape.size ** 2
+            }
+            return acc;
+        }, 0)
+    }
+
+}
+
+const calc = new AreaCalculator([
+    new Square(10),
+    new Circle(1),
+    new Circle(5)
+]);
+
+console.log('----------------------- ');
+console.log(calc.sum());
---
+++ README.md
@@ -0,0 +1,2 @@
+#learning by video:
+https://www.youtube.com/watch?v=xq13wiqvcTc&list=PL-MnVxzPI4zUt_0xwU3fnqCC4YKZkRtEO&index=26&t=2959s
