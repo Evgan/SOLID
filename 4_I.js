5814cb2eb9c2c34a51ac897536e852bc7917bdb1
epenkrart <e.penkrat@whiteskydigital.com>
2021-03-26 01:02:18 +0300

    4_I WITH USE interface segregation principle

--- 4_I.js
+++ 4_I.js
@@ -8,52 +8,50 @@ class Animal {
     constructor(name) {
         this.name = name
     }
+}
 
-    walk() {
-        console.log(`${this.name} can walk`);
-    }
-
+const swimer = {
     swim() {
         console.log(`${this.name} can swim`);
     }
 
+};
+
+const walker = {
+    walk() {
+        console.log(`${this.name} can walk`);
+    }
+};
+
+const flier = {
     fly() {
         console.log(`${this.name} can fly`);
     }
-}
+};
 
-class Dog extends Animal {
-    fly() {
-        return null; // Собаки не умеют летать, потому переопределяем метот с return null, НО ЭТО НАРУШЕНИЕ Interface segregation principle
-    }
-}
+class Dog extends Animal {}
+Object.assign(Dog.prototype, swimer, walker); // РЕАЛИЗАЦИЯ Interface segregation principle
 
-class Eagle extends Animal {
-    swim() {
-        super.swim();  // Орёл не умеют плавать, потому переопределяем метот с return null, НО ЭТО НАРУШЕНИЕ Interface segregation principle
-    }
-}
+class Eagle extends Animal {}
+Object.assign(Eagle.prototype, flier, walker); // РЕАЛИЗАЦИЯ Interface segregation principle
 
-class Whale extends Animal {
-    fly() {
-        return null; // Кит не умеют летать, потому переопределяем метот с return null, НО ЭТО НАРУШЕНИЕ Interface segregation principle
-    }
-}
+class Whale extends Animal {}
+Object.assign(Whale.prototype, swimer); // РЕАЛИЗАЦИЯ Interface segregation principle
 
 const dog = new Dog('Rax');
 
-dog.fly();
+// dog.fly(); у обхекта Класса Dog нет такого метода, так как мы такое пооведение не добавили через assign
 dog.swim();
 dog.walk();
 
 const eagle = new Eagle('Tim');
 
 eagle.fly();
-eagle.swim();
+// eagle.swim(); у обхекта Класса Dog нет такого метода, так как мы такое пооведение не добавили через assign
 eagle.walk();
 
 const whale = new Whale('Tom');
 
-whale.fly();
+// whale.fly(); у обхекта Класса Dog нет такого метода, так как мы такое пооведение не добавили через assign
 whale.swim();
-whale.walk();
+// whale.walk(); у обхекта Класса Dog нет такого метода, так как мы такое пооведение не добавили через assign
