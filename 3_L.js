3818f490eb60c72bc1fa3c03a4159a6e9feefde7
epenkrart <e.penkrat@whiteskydigital.com>
2021-03-25 14:04:47 +0300

    3_O WITH USE Liskov substitution principle

--- 3_L.js
+++ 3_L.js
@@ -4,31 +4,44 @@
 // Функции, которые используют базовый тип, должны уметь с ним взоимодействовать,
 // плюс взоимодействовать с подтипаними данного базового типа, при этом ничего не знать про это
 
+
+// #########################################################
+//                      ПРИМЕР 1
+// #########################################################
 class Person {
+}
+
+// Добавляем новую обстракцию:
+class Member extends Person {
     access() {
         console.log('----------------------- У тебя есть доступ');
     }
 }
 
-class Frontend extends Person {
+class Guest extends Person{
+    isGuest = true;
+}
+
+class Frontend extends Member {
     canCreateFrontend() {}
 }
 
-class Backend extends Person {
+class Backend extends Member {
     canCreateBackend() {}
 }
 
-// ЭТО НАРУШЕНИЕ ПРИНЦИПА: Liskov substitution principle
-class PersonFromDifferentCompany extends Person{
-    access() {
+
+class PersonFromDifferentCompany extends Guest{
+    access() {// ЭТО НАРУШЕНИЕ ПРИНЦИПА: Liskov substitution principle
         throw new Error ('У тебя нет доступа! Иди к себе!')
     }
 }
 
-function openSecretDoor(persone) {
+function openSecretDoor(member) {
     persone.access()
 }
 
 openSecretDoor(new Frontend());
 openSecretDoor(new Backend());
-openSecretDoor(new PersonFromDifferentCompany());
+// openSecretDoor(new PersonFromDifferentCompany()); // The should be member
+// То есть, суть сводится к тому, что мы сами должны правильно выбирать нужную обстракцию (слои) для extends
